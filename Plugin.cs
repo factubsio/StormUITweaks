80ce7deeaf12ca62beb9196504ed3e622054ff1a
factubsio <65080026+factubsio@users.noreply.github.com>
2022-08-19 22:29:34 +0100

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,98 @@
+﻿using BepInEx;
+using Eremite;
+using Eremite.Controller;
+using Eremite.Services;
+using Eremite.View.Cameras;
+using Eremite.View.Popups.GameMenu;
+using HarmonyLib;
+using System.Reflection;
+using TMPro;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace StormUITweaks
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private Harmony harmony;
+
+        private static Plugin Instance;
+        public static void LogInfo(object obj) => Instance.Logger.LogInfo(obj);
+
+        private void Awake()
+        {
+            Instance = this;
+
+            harmony = new Harmony("bubblestorm");
+
+            harmony.PatchAll(typeof(Plugin));
+
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+
+        }
+
+        private static Slider zoomSlider = null;
+        private static float maxZoomFactor = -20;
+        private static CameraController currentGameCam = null;
+
+        [HarmonyPatch(typeof(OptionsPopup), nameof(OptionsPopup.SetValues))]
+        [HarmonyPostfix]
+        private static void OptionsPopup_SetValues(OptionsPopup __instance)
+        {
+            if (Serviceable.PrefsService.HasKey("BubblePrefs.maxZoomFactor"))
+            {
+                maxZoomFactor = Serviceable.PrefsService.GetFloat("BubblePrefs.maxZoomFactor");
+                zoomSlider.value = -maxZoomFactor;
+            }
+        }
+
+        [HarmonyPatch(typeof(OptionsPopup), nameof(OptionsPopup.SetUpInputs))]
+        [HarmonyPostfix]
+        private static void OptionsPopup_SetUpInputs(OptionsPopup __instance)
+        {
+            zoomSlider.OnValueChangedAsObservable().Subscribe(newValue =>
+            {
+                maxZoomFactor = -newValue;
+                if (currentGameCam != null)
+                    currentGameCam.zoomLimit = new(maxZoomFactor, -8f);
+                Serviceable.PrefsService.SetFloat("BubblePrefs.maxZoomFactor", maxZoomFactor);
+            }).AddTo(__instance);
+        }
+
+        [HarmonyPatch(typeof(OptionsPopup), nameof(OptionsPopup.Initialize))]
+        [HarmonyPrefix]
+        private static void OptionsPopup_InitializePre(OptionsPopup __instance)
+        {
+            var prefab = __instance.cameraMouseSensitivitySlider.transform.parent.gameObject;
+
+            var mySlider = GameObject.Instantiate(prefab);
+            var rect = mySlider.transform as RectTransform;
+            rect.SetParent(prefab.transform.parent, true);
+            rect.localScale = Vector3.one;
+            rect.localPosition = new(0, rect.localPosition.y, 0);
+            rect.localRotation = Quaternion.identity;
+            rect.SetSiblingIndex(prefab.transform.GetSiblingIndex());
+
+            var slider = rect.GetComponentInChildren<Slider>();
+            slider.minValue = 20;
+            slider.maxValue = 50;
+            slider.value = 20;
+
+            zoomSlider = slider;
+
+            mySlider.transform.Find("Label").GetComponent<TextMeshProUGUI>().text = "Maximum zoom distance";
+        }
+
+        [HarmonyPatch(typeof(CameraController), nameof(CameraController.Start))]
+        [HarmonyPostfix]
+        private static void CameraController_Start(CameraController __instance)
+        {
+            __instance.zoomLimit = new(maxZoomFactor, -8f);
+            currentGameCam = __instance;
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,26 @@
+# StormUITweaks
+
+
+Quality of life, zero-balance, changes to Against the Storm.
+
+
+ * Adds an adjustable max camera zoom factor
+
+
+## Installation
+
+1. Download bepinx 5: https://github.com/BepInEx/BepInEx/releases/download/v5.4.21/BepInEx_x86_5.4.21.0.zip
+
+2. Unzip to your game directory, so it should look like:
+
+3. Run the game, this will populate some folders/config files, exit the game.
+
+4. Download the mod: 
+
+4. Put the mod .dll into the `Bepinx\Plugins\` folder, it should look like:
+
+
+
+
+
+
